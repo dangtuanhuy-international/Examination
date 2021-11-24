62b35a04fd4c060beebb4477a1806f1362b74f3f
HuyDang504 <huy.dang@bbv.vn>
2021-11-24 14:28:21 +0700

    Implement Serilog

--- src/Examination/Examination.API/Controllers/V1/ExamsController.cs
+++ src/Examination/Examination.API/Controllers/V1/ExamsController.cs
@@ -1,6 +1,7 @@
 using Examination.Application.Queries.V1.GetHomeExamList;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Examination.API.Controllers.V1
@@ -11,17 +12,20 @@ namespace Examination.API.Controllers.V1
     public class ExamsController : ControllerBase
     {
         private readonly IMediator _mediator;
-
-        public ExamsController(IMediator mediator)
+        private readonly ILogger<ExamsController> _logger;
+        public ExamsController(IMediator mediator, ILogger<ExamsController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetExamList()
         {
+            _logger.LogInformation("BEGIN: GetExamList");
             var query = new GetHomeExamListQuery();
             var queryResult = await _mediator.Send(query);
+            _logger.LogInformation("END: GetExamList");
             return Ok(queryResult);
         }
     }
