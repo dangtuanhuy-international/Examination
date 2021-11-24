bce7d1b97b4e50d349215c97ef9f197591508e73
HuyDang504 <huy.dang@bbv.vn>
2021-11-24 09:41:23 +0700

    Update

---
+++ src/Examination/Examination.Domain/AggregateModels/CategoryAggregate/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination.Domain.AggregateModels.CategoryAggregate
+{
+    public interface ICategoryRepository
+    {
+        Task<IList<Category>> GetExamListAsync();
+
+        Task<Category> GetExamByIdAsync(string id);
+    }
+}
